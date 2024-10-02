1ecb67d874d98af9af68ee9caa9b29b8d196e133
diogobnfr <diogobonofre@gmail.com>
2024-10-02 17:29:22 -0300

    :books: docs: update documentation comments

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,4 +1,21 @@
+"""This module provides a insertion sort algorithm."""
+
+
 def insertion_sort(list: list[int]) -> list[int]:
+    """
+    Sorts a list of integers in ascending order using the insertion sort algorithm.
+
+    This algorithm iterates over the list, and for each element, compares it with the
+    previous elements, shifting them as needed to ensure the current element is
+    inserted in the correct position. It continues this process until the entire
+    list is sorted.
+
+    Args:
+        list (list[int]): The list of integers to be sorted.
+
+    Returns:
+        list[int]: The sorted list of integers.
+    """
     for i in range(1, len(list)):
         while i > 0:
             if list[i] < list[i - 1]:
