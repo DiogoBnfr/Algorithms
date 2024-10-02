586171d355f9b915c575c31aff35bf01974646b7
diogobnfr <diogobonofre@gmail.com>
2024-10-02 17:24:10 -0300

    :recycle: refactor: more modern approach using with keyword

--- runtime.py
+++ runtime.py
@@ -6,7 +6,7 @@ import sys
 import time
 
 
-def measure_execution_time(filename: str, dirpath: str = "") -> None:
+def log(filename: str, root: str = "./", log_path: str = "") -> None:
     """
     Measures and logs the execution time of a Python script.
 
@@ -24,20 +24,19 @@ def measure_execution_time(filename: str, dirpath: str = "") -> None:
         None: The execution time is logged in a file with the `.out.txt` extension in the same
         directory.
     """
-    f = open(dirpath + filename + ".out.txt", "a")
+    with open(root + log_path + filename + ".out.txt", "a", encoding="utf_8") as file:
+        start_time = time.time()
+        subprocess.run(f"python {filename}.py", shell=True, check=False)
+        end_time = time.time()
 
-    start_time = time.time()
-    subprocess.run(f"python {filename}.py", shell=True)
-    end_time = time.time()
-
-    f.write(
-        f"{str(datetime.datetime.now()).split('.')[0]} "  # type: ignore
-        + f"ExecutionTime: {end_time - start_time:.10f}\n"
-    )
+    file.write(
+            f"{str(datetime.datetime.now()).split('.', maxsplit=1)[0]} "
+            + f"ExecutionTime: {end_time - start_time:.10f}\n"
+        )
 
 
 if __name__ == "__main__":
     try:
-        measure_execution_time(sys.argv[1])
+        log(sys.argv[1])
     except IndexError as e:
         print(f"error {e}".replace(" ", "::").upper())
